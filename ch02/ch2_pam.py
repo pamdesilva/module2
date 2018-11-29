d3a674a71f028272ea83c0d884433d027761540f
Pamela De Silva <pam.desilva@gmail.com>
2018-11-29 11:47:50 +0000

    initial commit

---
+++ ch02/ch2_pam.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 09:34:30 2018
+
+@author: pamde
+"""
+print(5 - 6)
+print(8 * 9)
+print(6 / 2)
+print(5 / 2)
+print(5.0 / 2)
+print(5 % 2)
+print(2 * (10 + 3))
+print(2**4)
+ 
+age = 5
+age = "almost there"
+age = 29.5
+age = 'I really don\'t know!'
+ 
+print(age)
+
+print ('hello' + 'world')
+print ("Joke \n" * 3)
+print ("Chen" + str(3))
+print ("hello".upper())
+print ("GOODBYE".lower())
+print ("\n \n")
+print ("the lord of the rings".title())
+print(type(10))
+
+print("Bob \n" * 2 + "Bob")
+
+S1 = 'hello ' + 'world '
+S2 = "Joke " * 3
+S3 = 5
+
+print(S1 + S2 * 10)
+print(S1 + S2 + str(S3))
+
+s1 = '4'
+s2 = '6'
+s3 = 8
+
+result = int(s1) + int(s2) + s3
+
+print(result)
+
+print("\n") 
+
+######## split strings
+
+str1 = 'a,b,c,d,e,happy'
+
+print(str1)
+
+splitStr = str1.split(',')
+
+print(splitStr)
+
+print("\n") 
+
+######## replace variables in strings
+
+age = 5
+like = "painting"
+color = "green"
+
+age_description = "My age is {} and I like {}.".format(age, like)
+print(age_description)
+
+age_description2 = "My age is {0} and I like {2}.".format(age, like, color)
+print(age_description2)
+
+print("\n") 
+
+test = """aaa
+bbb ccc ddd eee
+fff ggg hhh iii 
+jjj kkk lll
+"""
+
+print(test)
