4d0228be43b5a16922eb4e766248bdff7deeed59
Pamela De Silva <pam.desilva@gmail.com>
2018-12-13 16:49:18 +0000

    initial commit

---
+++ ch09/ch9_pam.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 13 09:28:48 2018
+
+@author: pamde
+"""
+###### Lists
+my_favourite_fruits = ['apple', 'orange', 'banana']
+#
+#x = ['this', 55, 'that', my_favourite_fruits]
+#
+#print(x[0])
+#print(x[1])
+#print(x[3][0])
+#print(x[-1])
+#print(x[-1][-2])
+#print(x[-2][-3])
+#
+#x.remove(my_favourite_fruits)
+#print(x)
+#
+#x[1] = 'and'
+#print(x)
+#
+#x.append('again')
+#print(x)
+#
+#x.append('muffins')
+#print(x)
+#
+#y = x
+#print(y)
+#
+#y = x.append('cupcakes')
+#print(y)
+#print(x)
+
+
+######## List operations: + *
+
+#x = ['the', 'cat', 'sat']
+#y = ['on', 'the', 'mat']
+#z = x + y
+#print(z)
+#z = x * 3
+#print(z)
+#print(type(z))
+#a = set(x + y)
+#print(a)
+#print(type(a))
+#print(x)
+#print(y)
+
+######### List operations: slice
+#
+#x = ['the', 'cat', 'sat', 'directly']
+#y = ['on', 'the', 'wet', 'mat']
+#z = x + y
+#print(x[0:3])
+#print(y[1:4])
+#print(z)
+#print(x)
+#print(z[-1])
+
+
+######## List operations: sort
+
+#x = ['the', 'cat', 'sat', 'directly']
+#y = ['on', 'the', 'wet', 'mat']
+#z = sorted(x)
+#print(z)
+#print(x)
+#
+#t = y.sort()
+#print(t)
+#print(y)
+
+
+######## List operations: reverse sort
+
+#x = [7, 11, 3, 9, 2]
+#print(sorted(x))
+#
+#print(sorted(x, reverse=True))
+#
+#print(x)
+#
+#y = [9, 4, 10, 29, 12]
+#
+#z = y.sort()
+#print('now z is: ', z)
+#print('now y is: ', y)
+#y.sort(reverse=True)
+#print('now y is: ',y)
+
+
+######## Tuples : immutable
+
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#del a[3]
+#print(a)
+#a[3] = 50
+#print(a)
+#a.append('z')
+#print(a)
+#
+b = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+##print(type(b))
+##del b[3]
+##print(b)
+##b[3] = 50
+##print(b)
+#print('b is: ', b)
+#c = list(b)
+#print('c is now: ', c)
+
+z = ['zs', 'yw', 'hd' 'cs', 'ab']
