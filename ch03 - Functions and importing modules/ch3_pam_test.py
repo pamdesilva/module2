f8133f3d239e783599efd23fc22c2362a1e51c44
Pamela De Silva <pam.desilva@gmail.com>
2018-12-19 14:04:42 +0000

    Added comments

--- ch03 - Functions and importing modules/ch3_pam_test.py
+++ ch03 - Functions and importing modules/ch3_pam_test.py
@@ -10,6 +10,30 @@ Created on Mon Dec  3 15:54:34 2018
 
 ###############################################
 
+#---------------------------------------
+#TASK 1: Input from user
+#---------------------------------------
+
+print('What\'s your name?')
+name = input().title()
+
+#name
+#print("Hello {}!".format(name.title()))
+
+#age
+print('What\'s your age?')
+age = input()
+
+#location
+print('Where do you come from?')
+location= input()
+
+print("Hello " + name + "! You are " + age +  ' years old. You come from ' + location + '.' )
+
+#---------------------------------------
+#Importing functions from an external file
+#---------------------------------------
+
 import ch3_pam_function as helpers
 
 helpers.convert_temp(80, 'k')
--- ch07 - Debugging/ch7_pam.py
+++ ch07 - Debugging/ch7_pam.py
@@ -39,3 +39,14 @@ def nestedOperation(result):
 result = simpleOperation(userInput)
 result2 = nestedOperation(result)
 print(result2)
+
+## debugging using breakpoints 
+#1) double click next to the line number of your code to set up a breakpoint,a red circle should
+#appear 
+#2)you can now run your code in debug mode using debugging buttons on the toolbar
+#  - first button is for you to start running your code until the break point.
+#  - second button allows you to run your code line-by-line until the breakpoint.
+#  - third one is for stepping into the sections (class and functions) 
+#  - fourth button is for you to step out when you feel that the error is not related to the current section
+#  - fifth button is for you to go to the next breakpoint
+#  - last, square shaped button is for you to exit debugging mode and go back to normal coding mode.
