9456cf242426ca957c3f1f392b478c4fcae87a19
Pamela De Silva <pam.desilva@gmail.com>
2018-12-19 09:31:00 +0000

    Updated ch01 file with comments

--- ch01/ch1_pam.py
+++ ch01/ch1_pam.py
@@ -5,5 +5,18 @@ Created on Wed Nov 28 15:44:52 2018
 @author: pamde
 """
 
-print('Hello')
+###################################
+
+#CHAPTER 01 - INTRODUCTION TO PYTHON
+
+###################################
+
+#---------------------------------------
+#Simple calculation operation in Python
+#---------------------------------------
 print(4 * 8 + 12)
+
+#-----------------------------
+#Print 'Hello world' in Python
+#-----------------------------
+print('Hello world!')
