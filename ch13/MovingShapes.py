2adbed4182d7e1bac5d1c2a68b4152154f96dc3c
Pamela De Silva <pam.desilva@gmail.com>
2018-12-21 14:38:13 +0000

    Added test file

---
+++ ch13/MovingShapes.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 19 14:27:57 2018
+
+@author: pamde
+"""
+
+from Shapes import *
+from pylab import random as r
+
+class MovingShape:
+    def __init__(self, frame, shape, diameter):
+        self.shape = shape
+        self.diameter = diameter
+        self.figure = Shape(shape, diameter)
+        self.x = 0
+        self.y = 0
+        self.dx = 10
+        self.dy = 10
+        self.goto(self.x, self.y)
+        
+    def goto(self, x, y):
+        self.figure.goto(x, y)
+        
+    def moveTick(self):
+        self.x += self.dx
+        self.y += self.dy
+        self.figure.goto(self.x, self.y)
+    
+class Square(MovingShape):
+    def __init__(self, frame, diameter):
+        MovingShape.__init__(self, frame, 'square', diameter)
+        
+class Diamond(MovingShape):
+    def __init__(self, frame, diameter):
+        MovingShape.__init__(self, frame, 'diamond', diameter)
+        
+class Circle(MovingShape):
+    def __init__(self, frame, diameter):
+        MovingShape.__init__(self, frame, 'circle', diameter)
+        
--- ch13/ch13_load_shapes.py
+++ ch13/ch13_load_shapes.py
@@ -11,3 +11,13 @@ frame = Frame()
 s1 = Shape('square', 100)
 s1.goto(200, 100)
 
+s2 = Shape('circle', 100)
+s2.goto(200, 100)
+
+x = 0
+y = 0
+
+for i in range(100):
+    s1.goto(x, y)
+    x = x + 5
+    y = y + 5
